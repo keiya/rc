222c484f530f44943a03217acc398ef679ccafaa
keiya <keiya_21@yahoo.co.jp>
2016-04-20 16:15:36 +0900

    automatic install

--- my_environment.sh
+++ my_environment.sh
@@ -5,3 +5,4 @@ git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 ln -s $HOME/repos/rc/.tmux.conf $HOME/
 ln -s $HOME/repos/rc/.toprc $HOME/
 ln -s $HOME/repos/rc/.vimrc $HOME/
+vim +PluginInstall +qall
