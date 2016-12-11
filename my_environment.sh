ae418fd9311fd7b2378e6804227b3df74871751f
keiya <keiya_21@yahoo.co.jp>
2016-12-12 05:00:36 +0900

    ssh config

--- my_environment.sh
+++ my_environment.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 cd $HOME
-mkdir -p repos/
 git clone https://github.com/keiya/rc.git $HOME/repos/rc
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 ln -s $HOME/repos/rc/.tmux.conf $HOME/
 ln -s $HOME/repos/rc/.toprc $HOME/
 ln -s $HOME/repos/rc/.vimrc $HOME/
+mkdir -p $HOME/.ssh/
+chmod 700 .ssh
+ln -s $HOME/repos/rc/ssh_config $HOME/.ssh/config
 vim +PluginInstall +qall
